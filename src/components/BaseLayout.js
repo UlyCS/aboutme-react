27b1fc92db3260142f9768fd4aef0e5e18e6f072
John Ulysses Paruginog <202011029@gordoncollege.edu.ph>
2022-12-22 18:43:13 +0800

    Updated Footer with hyperlink to email

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -31,8 +31,8 @@ export default function BaseLayout() {
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                     py={'1.5rem'} sx={{opacity: 0.7}} width={'100%'}>
-                  <p>template created with &hearts; by <a href={'https://paytonpierce.dev'}>Payton Pierce</a></p>
-                  <p>&copy; 2022</p>
+                  <p>John Ulysses N. Paruginog | BSCS-3A |<a href={'https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSBnpqWvgCPcJNgdsRtFXHkRGMHVMsFcHwkVGlkfknLVBLTkWVSXqHZmhsMrlHkpvsqfjSnF'}> 202011029@gordoncollege.edu.ph</a></p>
+                  <p>&copy; All Rights Reserved</p>
                </Box>
             </Grid>
          </Grid>
